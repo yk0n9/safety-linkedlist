50276a7314b52edac59a002a76be65d485602e9e
ykong1337 <ykonghao@icloud.com>
2023-06-29 11:15:44 +0800

    update data structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,321 +1,328 @@
-use std::cell::RefCell;
-use std::fmt::Debug;
-use std::rc::Rc;
+use std::fmt::Formatter;
+use std::ops::{Index, IndexMut};
 
-type Data<T> = Rc<RefCell<Node<T>>>;
+#[derive(Debug, Clone)]
+struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+    len: usize,
+}
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node<T> {
     data: T,
-    next: Option<Data<T>>,
+    next: Option<Box<Node<T>>>,
 }
 
 impl<T> Node<T> {
-    fn new(data: T) -> Rc<RefCell<Node<T>>> {
-        Rc::new(RefCell::new(Self { data, next: None }))
+    #[inline]
+    pub fn new(data: T) -> Box<Self> {
+        Box::new(Self { data, next: None })
+    }
+
+    #[inline]
+    pub fn as_ref(&self) -> &T {
+        &self.data
+    }
+
+    #[inline]
+    pub fn as_mut(&mut self) -> &mut T {
+        &mut self.data
     }
 }
 
-#[derive(Debug)]
-pub struct LinkedList<T> {
-    head: Option<Data<T>>,
-    last: Option<Data<T>>,
-    length: usize,
-}
-
-impl<T: Clone + Debug> LinkedList<T> {
+impl<T> LinkedList<T> {
+    #[inline]
     pub fn new() -> Self {
-        Self {
-            head: None,
-            length: 0,
-            last: None,
-        }
-    }
-
-    pub fn length(&self) -> usize {
-        self.length
-    }
-
-    pub fn clear(&mut self) -> &mut Self {
-        self.head = None;
-        self.length = 0;
-        self.last = None;
-
-        self
-    }
-
-    pub fn reverse(&mut self) -> &mut Self {
-        if self.length <= 1 {
-            return self;
-        }
-
-        let mut now = self.head.take();
-        while let Some(n) = now.as_mut() {
-            let next = n.borrow_mut().next.take();
-            self.prepend(n.borrow().data.clone());
-            self.length -= 1;
-            now = next;
-        }
-
-        self
-    }
-
-    pub fn front(&self) -> Option<T> {
-        match self.head.as_ref() {
-            None => None,
-            Some(n) => Some(n.borrow().data.clone()),
-        }
-    }
-
-    pub fn last(&self) -> Option<T> {
-        match self.last.as_ref() {
-            None => None,
-            Some(n) => Some(n.borrow().data.clone()),
-        }
-    }
-
-    pub fn rm_front(&mut self) -> &mut Self {
-        let tmp = match self.head.as_ref() {
-            None => return self,
-            Some(n) => n.borrow_mut().next.take(),
-        };
-        self.head = tmp;
-
-        self.length -= 1;
-        self
-    }
-
-    pub fn rm_last(&mut self) -> &mut Self {
-        if self.last.is_none() {
-            return self;
-        }
-
-        if self.length == 1 {
-            self.rm_front();
-            return self;
-        }
-
-        let next = match self.head.as_ref() {
-            None => return self,
-            Some(n) => range(n.clone(), self.length - 2),
-        };
-        next.borrow_mut().next = None;
-        self.last = Some(next.clone());
-
-        self.length -= 1;
-        self
+        Self { head: None, len: 0 }
     }
 
     pub fn append(&mut self, data: T) -> &mut Self {
         let new_node = Node::new(data);
-        match self.last.take() {
-            Some(node) => node.borrow_mut().next = Some(new_node.clone()),
-            None => self.head = Some(new_node.clone()),
+        let mut ptr = &mut self.head;
+        while let Some(node) = ptr {
+            ptr = &mut node.next;
         }
-        self.length += 1;
-        self.last = Some(new_node);
-
+        let _ = ptr.insert(new_node);
+        self.len += 1;
         self
     }
 
     pub fn prepend(&mut self, data: T) -> &mut Self {
-        let new_node = Node::new(data);
-        match self.head.take() {
-            Some(node) => {
-                new_node.borrow_mut().next = Some(node);
-                self.head = Some(new_node);
-            }
-            None => {
-                self.head = Some(new_node.clone());
-                self.last = Some(new_node)
-            }
+        let mut new_node = Node::new(data);
+        if self.head.is_some() {
+            let old = self.head.take();
+            new_node.next = old;
         }
-        self.length += 1;
-
+        self.head = Some(new_node);
+        self.len += 1;
         self
     }
 
-    pub fn rm(&mut self, index: usize) -> &mut Self {
-        if index >= self.length {
-            return self;
-        }
-        if index == 0 {
-            self.rm_front();
-            return self;
-        }
-        if index == self.length - 1 {
-            self.rm_last();
-            return self;
-        }
-
-        let next = match self.head.as_ref() {
-            None => return self,
-            Some(n) => range(n.clone(), index - 1),
-        };
-        let tmp = match next.borrow().next.as_ref() {
-            None => return self,
-            Some(n) => n.clone(),
-        };
-        next.borrow_mut().next = tmp.borrow_mut().next.take();
-
-        self.length -= 1;
-        self
-    }
-
-    pub fn set(&mut self, index: usize, data: T) -> &mut Self {
-        if index >= self.length {
-            return self;
-        }
-
-        let next = match self.head.as_ref() {
-            None => return self,
-            Some(n) => range(n.clone(), index),
-        };
-        next.borrow_mut().data = data;
-
-        self
-    }
-
-    pub fn get(&self, index: usize) -> Option<T> {
-        if index >= self.length {
-            return None;
-        }
-
-        let next = match self.head.as_ref() {
-            None => return None,
-            Some(n) => range(n.clone(), index),
-        };
-
-        let res = Some(next.borrow().data.clone());
-        res
-    }
-
-    pub fn insert(&mut self, index: usize, data: T) -> &mut Self {
-        if index > self.length {
-            return self;
-        }
-
-        if index == 0 {
-            self.prepend(data);
-            return self;
-        }
-
-        let new_node = Node::new(data);
-
-        let next = match self.head.as_ref() {
-            None => return self,
-            Some(n) => range(n.clone(), index - 1),
-        };
-
-        let tmp = match next.borrow().next.as_ref() {
-            None => return self,
-            Some(n) => n.clone(),
-        };
-        new_node.borrow_mut().next = Some(tmp.clone());
-        next.borrow_mut().next = Some(new_node.clone());
-
-        self.length += 1;
+    pub fn clear(&mut self) -> &mut Self {
+        self.head = None;
+        self.len = 0;
         self
     }
 
     pub fn is_empty(&self) -> bool {
-        if self.length > 0 {
-            return false;
+        self.head.is_none()
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn first(&self) -> Option<&T> {
+        return if let Some(ref node) = self.head {
+            Some(node.as_ref().as_ref())
+        } else {
+            None
+        };
+    }
+
+    pub fn first_mut(&mut self) -> Option<&mut T> {
+        return if let Some(ref mut node) = self.head {
+            Some(node.as_mut().as_mut())
+        } else {
+            None
+        };
+    }
+
+    pub fn last(&self) -> Option<&T> {
+        let mut ptr = &self.head;
+        if ptr.is_none() {
+            return None;
         }
-        true
+        while let Some(node) = ptr {
+            if node.next.is_none() {
+                break;
+            } else {
+                ptr = &node.next;
+            }
+        }
+        Some(ptr.as_deref().unwrap().as_ref())
+    }
+
+    pub fn last_mut(&mut self) -> Option<&mut T> {
+        let mut ptr = &mut self.head;
+        if ptr.is_none() {
+            return None;
+        }
+        loop {
+            if let Some(node) = ptr {
+                ptr = &mut node.next;
+            } else {
+                break;
+            }
+        }
+        Some(ptr.as_deref_mut().unwrap().as_mut())
+    }
+
+    pub fn pop_front(&mut self) -> &mut Self {
+        if self.head.is_none() {
+            return self;
+        }
+        let ptr = self.head.take();
+        self.head = ptr.unwrap().next;
+        self.len -= 1;
+        self
+    }
+
+    pub fn pop_last(&mut self) -> &mut Self {
+        if self.head.is_none() {
+            return self;
+        }
+        if self.len == 1 {
+            self.pop_front();
+            return self;
+        }
+        let mut ptr = &mut self.head;
+        for _ in 0..self.len - 2 {
+            if let Some(node) = ptr {
+                ptr = &mut node.next;
+            } else {
+                break;
+            }
+        }
+        ptr.as_deref_mut().unwrap().next = None;
+        self.len -= 1;
+        self
+    }
+
+    pub fn insert(&mut self, data: T, index: usize) -> &mut Self {
+        if index >= self.len {
+            self.append(data);
+            return self;
+        }
+        if index == 0 {
+            self.prepend(data);
+            return self;
+        }
+        let mut new_node = Node::new(data);
+        let mut ptr = &mut self.head;
+        for _ in 0..index - 1 {
+            if let Some(node) = ptr {
+                ptr = &mut node.next;
+            } else {
+                return self;
+            }
+        }
+        new_node.next = ptr.as_deref_mut().unwrap().next.take();
+        ptr.as_deref_mut().unwrap().next = Some(new_node);
+        self.len += 1;
+        self
+    }
+
+    pub fn reverse(&mut self) -> &mut Self {
+        if self.len <= 1 {
+            return self;
+        }
+        let mut ptr = self.head.take();
+        while let Some(mut node) = ptr {
+            ptr = node.next.take();
+            node.next = self.head.take();
+            self.head = Some(node);
+        }
+        self
     }
 
     pub fn iter(&self) -> Iter<'_, T> {
         Iter {
-            head: &self.head,
-            length: self.length,
-            index: 0,
+            ptr: self.head.as_ref(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            ptr: self.head.as_mut(),
+        }
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter {
+            ptr: self.head
         }
     }
 }
 
-fn range<T>(item: Data<T>, index: usize) -> Data<T> {
-    let mut next = item.clone();
-    for _ in 0..index {
-        let tmp = match next.borrow().next.as_ref() {
-            None => return item,
-            Some(n) => n.clone(),
-        };
-        next = tmp;
+struct Iter<'a, T> {
+    ptr: Option<&'a Box<Node<T>>>,
+}
+
+struct IterMut<'a, T> {
+    ptr: Option<&'a mut Box<Node<T>>>,
+}
+
+struct IntoIter<T> {
+    ptr: Option<Box<Node<T>>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(node) = self.ptr {
+            self.ptr = node.next.as_ref();
+            return Some(&node.data);
+        }
+        None
     }
-    next
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(node) = self.ptr.take() {
+            self.ptr = node.next.as_mut();
+            return Some(&mut node.data);
+        }
+        None
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(node) = self.ptr.take() {
+            self.ptr = node.next;
+            return Some(node.data);
+        }
+        None
+    }
+}
+
+impl<T> Index<usize> for LinkedList<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        let mut ptr = &self.head;
+        for _ in 0..index {
+            if let Some(node) = ptr {
+                ptr = &node.next;
+            } else {
+                break;
+            }
+        }
+        ptr.as_deref().unwrap().as_ref()
+    }
+}
+
+impl<T> IndexMut<usize> for LinkedList<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        let mut ptr = &mut self.head;
+        for _ in 0..index {
+            if let Some(node) = ptr {
+                ptr = &mut node.next;
+            } else {
+                break;
+            }
+        }
+        ptr.as_deref_mut().unwrap().as_mut()
+    }
 }
 
 impl<T: std::fmt::Display> std::fmt::Display for LinkedList<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         if self.head.is_none() {
             write!(f, "None")?;
         } else {
-            let mut next = self.head.clone();
-            while let Some(node) = next {
+            let mut ptr = &self.head;
+            while let Some(node) = ptr {
                 write!(
                     f,
                     "{} {}",
-                    node.borrow().data,
-                    match node.borrow().next.is_some() {
+                    node.data,
+                    match node.next.is_some() {
                         true => "-> ",
                         false => "",
                     }
                 )?;
-                next = node.borrow().next.clone();
+                ptr = &node.next;
             }
         }
         Ok(())
     }
 }
 
-impl<T: Debug + Clone> From<Vec<T>> for LinkedList<T> {
+impl<T> From<Vec<T>> for LinkedList<T> {
     fn from(value: Vec<T>) -> Self {
-        let mut head = LinkedList::<T>::new();
-        for i in value {
-            head.append(i);
-        }
-        head
-    }
-}
-
-impl<T: Debug + Clone> Into<Vec<T>> for LinkedList<T> {
-    fn into(self) -> Vec<T> {
-        let mut list: Vec<T> = vec![];
-        let len = self.length;
-        if len > 0 {
-            let mut next = self.head.clone();
-            while let Some(n) = next {
-                list.push(n.borrow().data.clone());
-                next = n.borrow().next.clone();
-            }
-            return list;
+        let mut list = LinkedList::new();
+        for data in value.into_iter() {
+            list.append(data);
         }
         list
     }
 }
 
-pub struct Iter<'a, T> {
-    head: &'a Option<Data<T>>,
-    length: usize,
-    index: usize,
-}
-
-impl<T: Debug + Clone> Iterator for Iter<'_, T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index == self.length {
-            None
-        } else {
-            let next = match self.head.as_ref() {
-                None => return None,
-                Some(n) => range(n.clone(), self.index)
-            };
-            self.index += 1;
-            let res = Some(next.borrow().data.clone());
-            res
+impl<T> Into<Vec<T>> for LinkedList<T> {
+    fn into(self) -> Vec<T> {
+        let mut list = vec![];
+        for data in self.into_iter() {
+            list.push(data);
         }
+        list
     }
 }
 
@@ -333,12 +340,12 @@ mod tests {
             .append(4)
             .prepend(0)
             .reverse()
-            .rm_front()
-            .rm_last();
-        assert_eq!(Some(3), link.get(3));
+            .pop_front()
+            .pop_last();
+        assert_eq!(3, link[3]);
 
-        let mut iter = link.iter();
-        assert_eq!(iter.next(), Some(3));
+        let mut iter = link.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
 
         let list: Vec<i32> = link.into();
         assert_eq!(vec![3, 2, 1, 3, 2, 1], list);
